285f2f7dbad5b5c8adb480c5851289ad0474a991
Naomi <3190111055@zju.edu.cn>
2020-03-20 02:28:18 +0800

    Complete

--- Practical 6/List sorting and filtering.py
+++ Practical 6/List sorting and filtering.py
@@ -4,4 +4,13 @@ Created on Fri Mar 20 01:26:10 2020
 
 @author: Naomi
 """
-
+#filtering
+data=list(map(int,input('gene_lengths=').split()))
+data.remove(max(data))
+data.remove(min(data))
+print(data)
+#Draw a boxplot
+import matplotlib.pyplot as plt
+plt.boxplot(x=data, labels=[''],showmeans=True)
+plt.title('gene lengths')
+plt.show()
