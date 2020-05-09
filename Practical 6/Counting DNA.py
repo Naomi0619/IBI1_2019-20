19e4b75267eb1630356022f584ed156701c3f7c5
Naomi <3190111055@zju.edu.cn>
2020-05-09 17:26:02 +0800

    Improvement of Practical 6

--- Practical 6/Counting DNA.py
+++ Practical 6/Counting DNA.py
@@ -5,17 +5,28 @@ Created on Fri Mar 20 00:17:14 2020
 @author: Naomi
 """
 
-#Data to plot
+#Import the number of four nucleotides
 a=int(input('A='))
 b=int(input('G='))
 c=int(input('C='))
 d=int(input('T='))
+#Count the frequency table
+s={'A':"%1.1f%%"%(a/21*100),'G':"%1.1f%%"%(b/21*100),'C':"%1.1f%%"%(c/21*100),'T':"%1.1f%%"%(d/21*100)}
+#Print the frequency table
+print('The frequency table:',s)
+#Import matplotlib
 import matplotlib.pyplot as plt
+#Labels of the pie
 labels=['A','G','C','T']
+#Data for the pie
 count=[a,b,c,d]
+#Colors of the pie
 colors=['lightsalmon','wheat','lightcyan','y']
-#Plot
+#Plot the pie
 plt.pie(count, labels=labels, colors=colors, autopct='%1.1f%%', shadow=False)
+#Title of the pie
 plt.title('pie of the four DNA nucleotides')
+#Make the pie circular
 plt.axis('equal')
+#Print the pie
 plt.show()
--- Practical 6/List sorting and filtering.py
+++ Practical 6/List sorting and filtering.py
@@ -4,13 +4,20 @@ Created on Fri Mar 20 01:26:10 2020
 
 @author: Naomi
 """
-#filtering
+#Import the ten gene lengths to the list
 data=list(map(int,input('gene_lengths=').split()))
+#Sort the list
+data.sort()
+#Remove the maximum and minimum
 data.remove(max(data))
 data.remove(min(data))
-print(data)
-#Draw a boxplot
+#Print the list
+print('List of sorted values without the most value:',data)
+#Import the matplotlib
 import matplotlib.pyplot as plt
+#Draw the boxplot
 plt.boxplot(x=data, labels=[''],showmeans=True)
+#Title of the boxplot
 plt.title('gene lengths')
+#Print the boxplot
 plt.show()
