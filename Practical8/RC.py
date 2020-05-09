d81c5e4d2f7e91b77c4fb1e8a5a3779351e9c1f1
Naomi <3190111055@zju.edu.cn>
2020-05-09 17:44:19 +0800

    Reverse complementary sequence

--- Practical8/RC.py
+++ Practical8/RC.py
@@ -6,4 +6,27 @@ Created on Wed Apr  1 21:02:33 2020
 """
 
 seq ='ATGCGACTACGATCGAGGGCCAT'
-print(seq[::-1])
+#Define an empty string to store the complementary sequence
+cDNA=""
+#Loop through the sequence re
+for i in seq:
+    #if the character is 'A'
+    if i=="A":
+         #Store 'T' to the string cDNA
+        cDNA+="T"
+    #else if the character is 'T'  
+    elif i=="T":
+        #Store 'A' to the string cDNA
+        cDNA+="A"
+    #else if the character is 'C'
+    elif i=="C":
+        #Store 'G' to the string cDNA
+        cDNA+="G"
+    #else if the character is 'G'
+    elif i=="G": 
+         #Store 'C' to the string cDNA
+        cDNA+="C"
+#Reverse the complementary sequence
+cDNA=cDNA[::-1]
+#Print the reverse complementary sequence
+print("The reverse complementary sequence:",cDNA)
